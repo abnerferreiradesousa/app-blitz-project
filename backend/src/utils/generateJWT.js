fb54e3dd76f4f5062a5f540f1b342a102e87780e
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-07-01 18:29:32 -0300

    :sparkles: Generate JWT
    Criando função que gera um token JWT.

--- backend/src/utils/generateJWT.js
+++ backend/src/utils/generateJWT.js
@@ -1,6 +1,13 @@
+const jwt = require('jsonwebtoken');
+
 const generateJWT = (payload) => {
   const jwtConfig = {
     expiresIn: '3d',
     algorithm: 'HS256',
   };
+
+  const token = jwt.sign({ data: payload }, process.env.SECRET_KEY, jwtConfig);
+  return token;
 };
+
+module.exports = generateJWT;
