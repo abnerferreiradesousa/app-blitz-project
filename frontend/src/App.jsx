3c136d753f1eb1326af7885b2af5a5e764edfc3d
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-07-04 08:50:34 -0300

    :heavy_plus_sign: [feat]: Eslint Frontend
    Adicionando e configurando eslint do Frontend.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+import './App.css'
+
+function App () {
+  const api = async () => {
+    const response = await fetch('http://localhost:3001/task')
+    const data = await response.json()
+    console.log(data.data)
+  }
+
+  useEffect(() => {
+    async function fetchFrom () {
+      await api()
+    }
+    fetchFrom()
+  }, [])
+
+  return (
+    <section>Home</section>
+  )
+}
+
+export default App
